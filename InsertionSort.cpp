9733ec64b88dbf9c23a41064c9dffbbe09786542
ZizahSwiftie <azizahulnha@gmail.com>
2025-03-26 22:51:37 +0700

    initialize

---
+++ InsertionSort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; // Membuat Array dengan panjang data 20
+int n; // Membuat variabel n
+
+void input()
+{ // Procedure Input
+while (true)
+{
+cout << "Masukkan Jumlah Data pada Array :" // Membuat Inputan jumlah element Array
+cin >> n; // memanggil variable inputan n
